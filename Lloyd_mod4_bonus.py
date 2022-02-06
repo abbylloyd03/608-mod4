3db1f5d2604e21895e3853165d49e10b2f2b3ffc
abbylloyd03 <abbylloyd03@gmail.com>
2022-02-06 13:35:31 -0600

    Update Module 4 Bonus

--- Lloyd_mod4_bonus.py
+++ Lloyd_mod4_bonus.py
@@ -1,38 +1,87 @@
-#Lloyd_mod4_bonus.py
-"""Read a text file and count the words."""
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
 
 with open('olympics.txt') as f:
     contents = f.read()
 
-contents_lower = contents.lower()
+
+# In[2]:
+
 
 words_counts = {}
 
 punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 rem_words = ['is', 'and', 'or', 'the', 'a']
 
-for x in contents_lower:
-    if x in punc:
-        contents_lower = contents_lower.replace(x, '')
+
+# In[3]:
+
+
+contents_lower = contents.lower()
+#print(contents_lower)
+
+
+# In[4]:
+
+
+for ele in contents_lower:
+    if ele in punc:
+        contents_lower= contents_lower.replace(ele, '')
+
+
+# In[5]:
+
+
+#print(contents_lower)
+
+
+# In[6]:
+
 
 contents_split = contents_lower.split()
+#print(contents_split)
+
+
+# In[7]:
+
 
 for word in contents_split:
     if word in rem_words:
-       contents_split.remove(word)
+        contents_split.remove(word)
 
-# count occurences of each unique word
+
+# In[8]:
+
+
+#print(contents_split)
+
+
+# In[9]:
+
+
 for word in contents_split:
     if word in words_counts:
         words_counts[word] += 1 # update existing key-value pair
     else:
         words_counts[word] = 1 # insert new key-value pair
 
-print(f'{"WORD":<12}COUNT')
 
-for word, count in sorted(words_counts.items()):
+# In[10]:
+
+
+#print(words_counts.items())
+
+
+# In[13]:
+
+
+print(f'{"COUNT":<12}WORD')
+
+for count, word in sorted(words_counts.items(), key=lambda item: item[1], reverse=True):
     print(f'{word:<12}{count}')
     
 print('\nNumber of unique words:', len(words_counts))
 
-    
